ecc81ba4692bada403b3f850730386c0978a564f
deon <deonjoub@gmail.com>
2016-11-29 13:53:16 +0200

    Small change to test code to verify that new config code works on the cameras

--- test_sensors.py
+++ test_sensors.py
@@ -6,14 +6,14 @@
 import unittest
 
 from tests.test_trusense_altimeter import TestDeviceTruSense
-# from tests.test_canon6d_cam import TestDeviceCanon6DCam
+from tests.test_canon6d_cam import TestDeviceCanon6DCam
 from tests.test_configure import TestConfigure
 from tests.test_session_logger import TestSessionLogger
 
 DO_INTERACTIVE_TESTS = False
 
-# if DO_INTERACTIVE_TESTS is True:
-#     from tests.test_canon6d_cam import TestInteractiveCanon6DCam
+if DO_INTERACTIVE_TESTS is True:
+    from tests.test_canon6d_cam import TestInteractiveCanon6DCam
 
 if __name__ == '__main__':
     unittest.main()

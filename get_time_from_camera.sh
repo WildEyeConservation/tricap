7af05807db7467e38db832fab5c1a9c584720f5a
Deon Joubert <deonjoub@gmail.com>
2017-02-23 14:59:18 +0200

    If no cameras are connected, you will still be able to startup the system.

--- get_time_from_camera.sh
+++ get_time_from_camera.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 DATETIME=`gphoto2 --get-config datetime`
 if [ $? -ne 0 ]; then
-    exit 1
+    echo "some problem with gphoto2, probably no cameras connected"
+    exit 0
 fi
 
 EPOCH=`echo "$DATETIME" | grep Current | awk '{print $2}'`
 if [[ ! $EPOCH =~ [0-9]{10} ]]; then
         echo "gphoto2 did not return a usable time."
-        exit 1
+        exit 0
 fi
 
 NEWTIME=`date --date @$EPOCH`

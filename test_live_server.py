7151730ffaafee968430a1786649cb915d5773d1
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-11 09:23:39 +0200

    added test_live_server

---
+++ test_live_server.py
@@ -0,0 +1,8 @@
+"""Behaviour tests for tricap. These take longer."""
+
+import unittest
+
+from tests.test_page_home_live_server import TestLiveServerHome
+
+if __name__ == '__main__':
+    unittest.main()

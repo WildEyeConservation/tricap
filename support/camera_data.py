538c8faff37ddbfed09007829439fc5905500765
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-17 09:05:27 +0200

    More efficient camera parsing

--- support/camera_data.py
+++ support/camera_data.py
@@ -2,76 +2,6 @@
 import gphoto2 as gp
 
 
-class ParseCamera():
-    def __init__(self):
-        self.context = gp.Context()
-        self.camera = gp.Camera()
-        self.camera.init(self.context)
-        self.text = self.camera.get_summary(self.context)
-        self.tex = str(self.text)
-        self.parse = ""
-
-    def parsing(self, data):
-        self.parse = ""
-        self.index = self.tex.find(data) + len(data)
-        while self.tex[self.index] != ' ' and self.tex[self.index] != '\n':
-            self.parse += str(self.tex[self.index])
-            self.index += 1
-        return self.parse
-
-    def parse_serial_number(self):
-        self.serial_number = self.parsing("Serial Number: ")
-        return self.serial_number
-
-    def parse_available_space(self):
-        self.available_space = self.parsing("Free Space (Bytes): ")
-        self.available_space = int(self.available_space)/(1024*1024)
-        return str(self.available_space) + " Mb"
-
-    def parse_battery_level(self):
-        self.serial_number = self.parsing("value: ")
-        return self.serial_number.strip("%")
-
-# Pi test code
-# camera_data = ParseCamera()
-# print(camera_data.parse_available_space())
-# print(camera_data.parse_serial_number())
-# print(camera_data.parse_battery_level())
-
-# context = gp.Context()
-# camera_list = []  # make a list of all available cameras
-# for name, addr in context.camera_autodetect():
-#     camera_list.append((name, addr))
-# if not camera_list:
-#     print('No camera detected')
-#
-# camera_list.sort(key=lambda x: x[0])
-# # ask user to choose one
-# for index, (name, addr) in enumerate(camera_list):
-#     print('{:d}:  {:s}  {:s}'.format(index, addr, name))
-# # choice = input('Please input number of chosen camera: ')
-# # choice = int(choice)
-# # if choice < 0 or choice >= len(camera_list):
-# #     print('Number out of range')
-#
-#     # initialise chosen camera
-# cameraNumber = 0
-#
-# for cameraNumber in range(3):
-#     name, addr = camera_list[cameraNumber]  # choice
-#     camera = gp.Camera()
-#     # search ports for camera port name
-#     port_info_list = gp.PortInfoList()
-#     port_info_list.load()
-#     idx = port_info_list.lookup_path(addr)
-#     camera.set_port_info(port_info_list[idx])
-#     camera.init(context)
-#     text = camera.get_summary(context)
-#     print('Summary')
-#     print('=======')
-#     print(str(text))
-
-
 class ParseData():
     def __init__(self):
         self.context = gp.Context()
@@ -85,42 +15,39 @@ class ParseData():
         # ask user to choose one
         for self.index, (self.name, self.addr) in enumerate(self.camera_list):
             print('{:d}:  {:s}  {:s}'.format(self.index, self.addr, self.name))
-        self.parse[0] = ""
-        self.parse[1] = ""
-        self.parse[2] = ""
+        self.parse = [None]*3
 
-    def parse_data(self, data):
-        for self.cameraNumber in range(3):
-            self.name, self.addr = self.camera_list[self.cameraNumber]  # choice
-            self.camera = gp.Camera()
-            # search ports for camera port name
-            self.port_info_list = gp.PortInfoList()
-            self.port_info_list.load()
-            self.idx = self.port_info_list.lookup_path(self.addr)
-            self.camera.set_port_info(self.port_info_list[self.idx])
-            self.camera.init(self.context)
-            self.text = self.camera.get_summary(self.context)
+    def parse_data(self, data, number):
+        self.name, self.addr = self.camera_list[number]  # choice
+        self.camera = gp.Camera()
+        # search ports for camera port name
+        self.port_info_list = gp.PortInfoList()
+        self.port_info_list.load()
+        self.idx = self.port_info_list.lookup_path(self.addr)
+        self.camera.set_port_info(self.port_info_list[self.idx])
+        self.camera.init(self.context)
+        self.text = self.camera.get_summary(self.context)
 
-            self.tex = str(self.text)
-            self.parse[self.cameraNumber] = ""
-            self.index = self.tex.find(data) + len(data)
-            while self.tex[self.index] != ' ' and self.tex[self.index] != '\n':
-                self.parse[self.cameraNumber] += str(self.tex[self.index])
-                self.index += 1
-        return self.parse
+        self.tex = str(self.text)
+        self.parse[number] = ""
+        self.index = self.tex.find(data) + len(data)
+        while self.tex[self.index] != ' ' and self.tex[self.index] != '\n':
+            self.parse[number] += str(self.tex[self.index])
+            self.index += 1
+        return self.parse[number]
 
     def parse_serial_number(self, number):
-        self.serial_number = self.parse_data("Serial Number: ")
-        return self.serial_number[number]
+        self.serial_number = self.parse_data("Serial Number: ", number)
+        return self.serial_number
 
     def parse_available_space(self, number):
-        self.available_space = self.parse_data("Free Space (Bytes): ")
-        self.available_space = int(self.available_space[number])/(1024*1024)
+        self.available_space = self.parse_data("Free Space (Bytes): ", number)
+        self.available_space = int(self.available_space)/(1024*1024)
         return str(self.available_space) + " Mb"
 
     def parse_battery_level(self, number):
-        self.serial_number = self.parse_data("value: ")
-        return self.serial_number[number].strip("%")
+        self.serial_number = self.parse_data("value: ", number)
+        return self.serial_number.strip("%")
 
 camera_data = ParseData()
 for nums in range(3):

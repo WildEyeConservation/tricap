28ca4cdb65d97e6016a7e1a4dba1f8ad1d350a63
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-17 08:18:39 +0200

    Parsing camera data separately

--- support/camera_data.py
+++ support/camera_data.py
@@ -85,7 +85,9 @@ class ParseData():
         # ask user to choose one
         for self.index, (self.name, self.addr) in enumerate(self.camera_list):
             print('{:d}:  {:s}  {:s}'.format(self.index, self.addr, self.name))
-        self.parse = [None]*3
+        self.parse[0] = ""
+        self.parse[1] = ""
+        self.parse[2] = ""
 
     def parse_data(self, data):
         for self.cameraNumber in range(3):
@@ -103,24 +105,25 @@ class ParseData():
             self.parse[self.cameraNumber] = ""
             self.index = self.tex.find(data) + len(data)
             while self.tex[self.index] != ' ' and self.tex[self.index] != '\n':
-                self.parse += str(self.tex[self.index])
+                self.parse[self.cameraNumber] += str(self.tex[self.index])
                 self.index += 1
         return self.parse
 
-    def parse_serial_number(self):
+    def parse_serial_number(self, number):
         self.serial_number = self.parse_data("Serial Number: ")
-        return self.serial_number
+        return self.serial_number[number]
 
-    def parse_available_space(self):
+    def parse_available_space(self, number):
         self.available_space = self.parse_data("Free Space (Bytes): ")
-        self.available_space = int(self.available_space)/(1024*1024)
+        self.available_space = int(self.available_space[number])/(1024*1024)
         return str(self.available_space) + " Mb"
 
-    def parse_battery_level(self):
+    def parse_battery_level(self, number):
         self.serial_number = self.parse_data("value: ")
-        return self.serial_number.strip("%")
+        return self.serial_number[number].strip("%")
 
 camera_data = ParseData()
-print(camera_data.parse_available_space())
-print(camera_data.parse_serial_number())
-print(camera_data.parse_battery_level())
+for nums in range(3):
+    print(camera_data.parse_available_space(nums))
+    print(camera_data.parse_serial_number(nums))
+    print(camera_data.parse_battery_level(nums))

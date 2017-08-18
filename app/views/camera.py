4848b7846367177ffb7ac32f4f2f072e65410ddc
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-18 11:09:20 +0200

    Added dummy camera test for camera details

--- app/views/camera.py
+++ app/views/camera.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, redirect, url_for, render_template, request
-from support.camera_data import ParseData
+
+from support.configure import TricapConfig
 
 camera_bp = Blueprint('camera', __name__)
+
+
+def using_dummy_camera():
+    triconfig = TricapConfig()
+    web_settings = triconfig.get_section_dict(TricapConfig.WEB_SECTION_HEADER)
+    dummy = web_settings['cams_required']
+    if dummy == "dummy":
+        return True
+    else:
+        return False
+
 
 @camera_bp.route('/camera')
 def camera():
@@ -9,11 +21,16 @@ def camera():
     free_space = [""]*3
     battery = [""]*3
 
-    camera_data = ParseData()
-    for camera_number in range(3):
-        # serial_number[camera_number], free_space[camera_number], battery[camera_number] = 1*(camera_number+1), \
-        #                                                                                   2*(camera_number+1), \
-        #                                                                                   3*(camera_number+1)
-        serial_number[camera_number], free_space[camera_number], battery[camera_number] = camera_data.parse_camera(camera_number)
+    if using_dummy_camera() == False:
+        from support.camera_data import ParseData
+        camera_data = ParseData()
+        for camera_number in range(3):
+            serial_number[camera_number], free_space[camera_number], battery[camera_number] = camera_data.parse_camera(camera_number)
+    else:
+        for camera_number in range(3):
+            serial_number[camera_number], free_space[camera_number], battery[camera_number] = 1*(camera_number+1), \
+                                                                                              2*(camera_number+1), \
+                                                                                              3*(camera_number+1)
+
     return render_template('/camera/camera.html', serial_number = serial_number, free_space = free_space,
                            battery = battery)

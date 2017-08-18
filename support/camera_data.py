05cc0be02c80455548e89fd75258b2211c6b3662
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-18 09:59:13 +0200

    Added a button to return to home and code cleanup

--- support/camera_data.py
+++ support/camera_data.py
@@ -4,33 +4,10 @@ from app import tricap_cameras
 
 
 class ParseData():
-    #def __init__(self):
-
-        # self.context = gp.Context()
-        # self.camera_list = []  # make a list of all available cameras
-        # for self.name, self.addr in self.context.camera_autodetect():
-        #     self.camera_list.append((self.name, self.addr))
-        # if not self.camera_list:
-        #     print('No camera detected')
-        #
-        # self.camera_list.sort(key=lambda x: x[0])
-        # # ask user to choose one
-        # # for self.index, (self.name, self.addr) in enumerate(self.camera_list):
-        # #     print('{:d}:  {:s}  {:s}'.format(self.index, self.addr, self.name))
-        # self.parse = [""]*3
 
     def parse_data(self, data, number):
-        # self.name, self.addr = self.camera_list[number]  # choice
-        # self.camera = gp.Camera()
-        # # search ports for camera port name
-        # self.port_info_list = gp.PortInfoList()
-        # self.port_info_list.load()
-        # self.idx = self.port_info_list.lookup_path(self.addr)
-        # self.camera.set_port_info(self.port_info_list[self.idx])
-        # self.camera.init(self.context)
         self.context = tricap_cameras[number].get_camera_context()
         self.text = tricap_cameras[number].get_camera().get_summary(self.context)
-        # self.text = self.camera.get_summary(self.context)
 
         self.tex = str(self.text)
         self.parse[number] = ""
@@ -54,17 +31,8 @@ class ParseData():
         return self.serial_number
 
     def parse_camera(self, number):
-        # self.name, self.addr = self.camera_list[number]  # choice
-        # self.camera = gp.Camera()
-        # # search ports for camera port name
-        # self.port_info_list = gp.PortInfoList()
-        # self.port_info_list.load()
-        # self.idx = self.port_info_list.lookup_path(self.addr)
-        # self.camera.set_port_info(self.port_info_list[self.idx])
-        # self.camera.init(self.context)
         self.context = tricap_cameras[number].get_camera_context()
         self.text = tricap_cameras[number].get_camera().get_summary(self.context)
-        # self.text = self.camera.get_summary(self.context)
 
         self.tex = str(self.text)
         self.parse = [""]*3
@@ -83,18 +51,3 @@ class ParseData():
 
         return self.parse[0], str(int(self.parse[1])/(1024*1024)) + " Mb", self.parse[2]
 
-
-# camera_data = ParseData()
-# serNum = ""
-# freeSpace = ""
-# batt = ""
-# for nums in range(3):
-#     serNum, freeSpace, batt = camera_data.parse_camera(nums)
-#     print(serNum)
-#     print(freeSpace)
-#     print(batt)
-
-
-    # print(camera_data.parse_available_space(nums))
-    # print(camera_data.parse_serial_number(nums))
-    # print(camera_data.parse_battery_level(nums))

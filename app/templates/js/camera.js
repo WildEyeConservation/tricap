176697defd2b8601864b7618a746d324193d30fa
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-21 16:21:21 +0200

    Added a refresh button to camera page

--- app/templates/js/camera.js
+++ app/templates/js/camera.js
@@ -2,13 +2,13 @@
 // All of the following code will only run when the page is ready
 //  (the $() is short for $(document).ready())
 
-var batteryWarning = 50 // Below 50 percent
-var spaceWarning = 145000 // Below +-145Gb
+var batteryWarning = 50; // Below 50 percent
+var spaceWarning = 145000; // Below +-145Gb
 //var battery0 = parseInt({{battery[0]}})
 
 $(function(){
     // Check for low battery
-    if({{battery[0]}} < batteryWarning){
+    if({{battery[0]}} < batteryWarning || {{free_space[0]}} < spaceWarning){
         $('#middle_camera').removeClass('alert-info');
         $('#middle_camera').addClass('alert-danger');
     } else {
@@ -16,7 +16,7 @@ $(function(){
         $('#middle_camera').addClass('alert-info');
     }
 
-    if({{battery[1]}} < batteryWarning){
+    if({{battery[1]}} < batteryWarning || {{free_space[1]}} < spaceWarning){
         $('#front_camera').removeClass('alert-info');
         $('#front_camera').addClass('alert-danger');
     } else {
@@ -24,31 +24,7 @@ $(function(){
         $('#front_camera').addClass('alert-info');
     }
 
-    if({{battery[2]}} < batteryWarning){
-        $('#back_camera').removeClass('alert-info');
-        $('#back_camera').addClass('alert-danger');
-    } else {
-        $('#back_camera').removeClass('alert-danger');
-        $('#back_camera').addClass('alert-info');
-    }
-
-    if({{free_space[0]}} < spaceWarning){
-        $('#middle_camera').removeClass('alert-info');
-        $('#middle_camera').addClass('alert-danger');
-    } else {
-        $('#middle_camera').removeClass('alert-danger');
-        $('#middle_camera').addClass('alert-info');
-    }
-
-    if({{free_space[1]}} < spaceWarning){
-        $('#front_camera').removeClass('alert-info');
-        $('#front_camera').addClass('alert-danger');
-    } else {
-        $('#front_camera').removeClass('alert-danger');
-        $('#front_camera').addClass('alert-info');
-    }
-
-    if({{free_space[2]}} < spaceWarning){
+    if({{battery[2]}} < batteryWarning || {{free_space[2]}} < spaceWarning){
         $('#back_camera').removeClass('alert-info');
         $('#back_camera').addClass('alert-danger');
     } else {
@@ -59,10 +35,10 @@ $(function(){
 //    $('#home_button').click(function{
 //        $('#home_button').css("color", "yellow");
 //    });
-
-    $("#home_button").hover(function(){
-        $(this).css("background-color", "yellow");
-        }, function(){
-        $(this).css("background-color", "orange");
-    });
+//
+//    $("#home_button").hover(function(){
+//        $(this).css("background-color", "yellow");
+//        }, function(){
+//        $(this).css("background-color", "orange");
+//    });
 });

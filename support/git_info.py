839616ebda128ab86759c7b0c47e15754b3969a6
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-25 09:50:35 +0200

    Updated GIt parse id

--- support/git_info.py
+++ support/git_info.py
@@ -1,15 +1,24 @@
 from subprocess import PIPE, Popen
 
-GIT_COMMIT_FIELDS = ['id', 'author_name', 'author_email', 'date', 'message']
 GIT_LOG_FORMAT = ['%H', '%an', '%ae', '%ad', '%s']
-
 GIT_LOG_FORMAT = '%x1f'.join(GIT_LOG_FORMAT) + '%x1e'
 
-git_parse = Popen('git log --format="%s"' % GIT_LOG_FORMAT, shell=True, stdout=PIPE)
-(log, _) = git_parse.communicate()
-print(log)
-log = log.strip('\n\x1e').split("\x1e")
-log = [row.strip().split("\x1f") for row in log]
-log = [dict(zip(GIT_COMMIT_FIELDS, row)) for row in log]
-print(log)
-print(log[0]['id'])
+class Git():
+    def __init__(self):
+        self.index = 0
+        self.git_parse = ""
+        self.parse_id = ""
+
+    def id(self):
+        self.git_parse = Popen('git log --format="%s"' % GIT_LOG_FORMAT, shell=True, stdout=PIPE)
+        (log, _) = self.git_parse.communicate()
+        log = str(log)
+        for self.index in range(2,42):
+            self.parse_id += log[self.index]
+        print(self.parse_id)
+
+parser = Git()
+parser.id()
+
+
+

68d06f5de3ee497c4a400cf8630588a41436629c
Pieter Botha <pieter.botha@innoventix.co.za>
2017-08-30 11:16:26 +0200

    SD card reader before test on Linux

--- SD_card_reader.py
+++ SD_card_reader.py
@@ -25,7 +25,7 @@ def copy_directory(src, dest):
 
 def copy_thread(drv, num):
     print(drv)
-    copy_directory(drive_list[num] + 'DCIM', drv + str(datetime.date.today()) + Sortie + '/images/Camera' + str(num))
+    copy_directory(os.path.join(drive_list[num], 'DCIM'), os.path.join(drv, str(datetime.date.today()), Sortie, 'images', 'Camera'+str(num)))
 
 
 def number_files(path):
@@ -35,12 +35,16 @@ def number_files(path):
 
 # ******************************************************************************************************************** #
 # Start the program
+print("Starting")
 drive = psutil.disk_partitions()
 for k in range(drive.__len__()):
     print(str(k) + ": " + str(drive[k]))
 
-print("Starting")
-# print(drive)
+internal = int(input("Choose internal drive from list above and enter number: "))
+drive[0], drive[internal] = drive[internal], drive[0]
+external = int(input("Choose internal drive from list above and enter number: "))
+drive[1], drive[external] = drive[external], drive[1]
+
 for i in range(drive.__len__()):
     if drive[i].fstype == 'exFAT':  # 'FAT32'
         drive_list.append(drive[i].device)
@@ -48,37 +52,32 @@ for i in range(drive.__len__()):
 # Change the drive_list based on the exif data serial number
 # Read exif data to determine which camera is used
 for j in range(drive_list.__len__()):
-    first_file = os.listdir(drive_list[j] + '/DCIM/100CANON')[0]  # Put in threads area
-    file = open(drive_list[j] + '/DCIM/100CANON/' + first_file, 'rb')
+    first_file = os.listdir(os.path.join(drive_list[j], 'DCIM', '100CANON'))[0]  # Put in threads area
+    file = open(os.path.join(drive_list[j], 'DCIM', '100CANON', first_file), 'rb')
     tags = exifread.process_file(file)
 
     if 'EXIF BodySerialNumber' in tags.keys():
         serial_number[j] = tags['EXIF BodySerialNumber']
-        print(serial_number[j])
 
-for j in range(drive_list.__len__()):
-    if str(serial_number[j]) == "032024003117":
-        serial_number[0], serial_number[j] = serial_number[j], serial_number[0]
-        # Change the drive list here
-    elif str(serial_number[j]) == "023052000180":
-        serial_number[1], serial_number[j] = serial_number[j], serial_number[1]
-    elif str(serial_number[j]) == "413051000325":
-        serial_number[2], serial_number[j] = serial_number[j], serial_number[2]
+if drive_list.__len__() == 3:
+    # Change the drive list here
+    for j in range(drive_list.__len__()):
+        if str(serial_number[j]) == "032024003117":
+            serial_number[0], serial_number[j] = serial_number[j], serial_number[0]
+            drive_list[0], drive_list[j] = drive_list[j], drive_list[0]
+        elif str(serial_number[j]) == "023052000180":
+            serial_number[1], serial_number[j] = serial_number[j], serial_number[1]
+            drive_list[1], drive_list[j] = drive_list[j], drive_list[1]
+        elif str(serial_number[j]) == "413051000325":
+            serial_number[2], serial_number[j] = serial_number[j], serial_number[2]
+            drive_list[2], drive_list[j] = drive_list[j], drive_list[2]
 print(serial_number)
 
-
-# first_file = os.listdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera0/100CANON')[0]  # Put in threads area
-# file = open(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera0/100CANON/' + first_file, 'rb')
-# tags = exifread.process_file(file)
-# if 'EXIF BodySerialNumber' in tags.keys():
-#     print(tags['EXIF BodySerialNumber'])
-
-
 for j in range(drive_list.__len__()):
-    path = drive_list[j] + 'DCIM/100CANON'
+    path = os.path.join(drive_list[j], 'DCIM', '100CANON')
     num_files1 = len([f for f in os.listdir(path)
                       if os.path.isfile(os.path.join(path, f))])
-    path = drive_list[j] + 'DCIM/101CANON'
+    path = os.path.join(drive_list[j], 'DCIM', '101CANON')
     num_files2 = len([f for f in os.listdir(path)
                       if os.path.isfile(os.path.join(path, f))])
     num_files += (num_files1 + num_files2)
@@ -86,18 +85,19 @@ for j in range(drive_list.__len__()):
 print(num_files)
 threads = []
 nums = 0
-Sortie = "/Sortie0"
+Sortie = "Sortie0"
 
-if os.path.isdir(drive[0].device + str(datetime.date.today()) + '/Sortie0'):
-    while os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie):
+if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), 'Sortie0')):
+    while os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie)):
         nums += 1
-        Sortie = "/Sortie" + str(nums)
+        Sortie = "Sortie" + str(nums)
+print(Sortie)
 
 for j in range(drive_list.__len__()):  # starts two threads per drive_list
     print(drive_list[j])
-    threads0 = threading.Thread(target=copy_thread, args=(drive[0].device, j,))  # Internal drive
+    # threads0 = threading.Thread(target=copy_thread, args=(drive[0].device, j,))  # Internal drive
     threads1 = threading.Thread(target=copy_thread, args=(drive[1].device, j,))  # External drive
-    threads0.start()
+    # threads0.start()
     threads1.start()
 
 print("Starting file copy")
@@ -106,17 +106,32 @@ time.sleep(2)
 bar = progressbar.ProgressBar()
 for i in bar(range(num_files)):
     i = 0
-    if os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera0/100CANON'):
-        i += number_files(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera0/100CANON')
-    if os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera1/100CANON'):
-        i += number_files(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera1/100CANON')
-    if os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera2/100CANON'):
-        i += number_files(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera2/100CANON')
-    if os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera0/101CANON'):
-        i += number_files(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera0/101CANON')
-    if os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera1/101CANON'):
-        i += number_files(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera1/101CANON')
-    if os.path.isdir(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera2/101CANON'):
-        i += number_files(drive[0].device + str(datetime.date.today()) + Sortie + '/images/Camera2/101CANON')
+    if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera0', '100CANON')):
+        i += number_files(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera0', '100CANON'))
+    if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera1', '100CANON')):
+        i += number_files(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera1', '100CANON'))
+    if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera2', '100CANON')):
+        i += number_files(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera2', '100CANON'))
+    if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera0', '101CANON')):
+        i += number_files(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera0', '101CANON'))
+    if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera1', '101CANON')):
+        i += number_files(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera1', '101CANON'))
+    if os.path.isdir(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera2', '101CANON')):
+        i += number_files(os.path.join(drive[1].device, str(datetime.date.today()), Sortie, 'images', 'Camera2', '101CANON'))
     bar.update(i)
     time.sleep(1)
+
+# Rename folder to make more sense for the user
+# for r in range(2):
+for r in range(1, 2):
+    if os.path.isdir(os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera0')):
+        os.rename(os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera0'),
+                  os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera Left'))
+    if os.path.isdir(os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera1')):
+        os.rename(os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera1'),
+                  os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera Centre'))
+    if os.path.isdir(os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera2')):
+        os.rename(os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera2'),
+                  os.path.join(drive[r].device, str(datetime.date.today()), Sortie, 'images', 'Camera Right'))
+
+print("Finished")

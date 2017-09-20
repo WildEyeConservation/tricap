5ab6e3d80f685d0230fc6c849fb13e28546f44dd
Pieter Botha <pieter.botha@innoventix.co.za>
2017-09-20 13:43:35 +0200

    Error resolved error start

--- app/views/home.py
+++ app/views/home.py
@@ -221,16 +221,19 @@ def _has_capture_started():
     config = TricapConfig()
 
     if not use_dummy_cams:
-        if altimeter.get_error_start() == True and tricap_manager.get_state() != CAM_MANAGER_STATES.STARTED:
-            print(1)
-            tricap_manager.start_capturing()
+        # if altimeter.get_error_start() == True and tricap_manager.get_state() != CAM_MANAGER_STATES.STARTED:
+        #     print(1)
+        #     tricap_manager.start_capturing()
+        #     return True
+        if tricap_manager.get_state() == CAM_MANAGER_STATES.STARTED:
             return True
 
         if str(altimeter.get_error()) != "":
             if str(altimeter.get_error()) == "01" and tricap_manager.get_state() != CAM_MANAGER_STATES.STARTED:
-                print(2)
+                start_err = True
                 tricap_manager.start_capturing()
                 return True
+
             else:
                 print(str(altimeter.get_error()))
 
